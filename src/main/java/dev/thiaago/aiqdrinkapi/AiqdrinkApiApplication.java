eb50efc7ccef349a7b054979067c71b8ba8f615f
Thiago Ferreira <thiaago.dev@gmail.com>
2025-03-29 12:16:59 -0300

    first commit

---
+++ src/main/java/dev/thiaago/aiqdrinkapi/AiqdrinkApiApplication.java
@@ -0,0 +1,13 @@
+package dev.thiaago.aiqdrinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiqdrinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiqdrinkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thiaago/aiqdrinkapi/AiqdrinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiaago.aiqdrinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiqdrinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
